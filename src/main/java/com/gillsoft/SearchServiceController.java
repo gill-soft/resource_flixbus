7b9ae1cfe8b2cfbd83e159a8faca927da23fedd2
kashpur <kashpur.info@gmail.com>
2020-03-23 13:49:21 +0200

    Fix

--- src/main/java/com/gillsoft/SearchServiceController.java
+++ src/main/java/com/gillsoft/SearchServiceController.java
@@ -54,7 +54,7 @@ public class SearchServiceController extends SimpleAbstractTripSearchService<Tri
 	private RestClient client;
 	
 	@Autowired
-	@Qualifier("MemoryCacheHandler")
+	@Qualifier("RedisMemoryCache")
 	private CacheHandler cache;
 
 	@Override
